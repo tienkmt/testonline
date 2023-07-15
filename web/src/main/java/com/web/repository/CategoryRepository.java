61ddb9d653fff8581ba06fcf67bead97f5c1503d
unknown <tienkmt21@gmail.com>
2023-07-15 13:52:09 +0700

    up code

--- web/src/main/java/com/web/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.web.repository;
-
-import com.web.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
